2380a6a8b8eb0dd71fa864c64f4fe041a2db05f8
s_iceman <s_iceman@protonmail.com>
2023-01-12 22:27:29 +0300

    feat: add basic configs

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+import path from "path";
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/start.ts'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
